64a5858c91ecd52fee0834ca536e03630f6f726f
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-05-08 14:48:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,5 +25,17 @@ end
   
 end
 
+Scraper.new.print_courses
+
+  #def print_courses
+   # self.make_courses
+    #Course.all.each do |course|
+      #if course.title && course.title != ""
+        #puts "Title: #{course.title}"
+        #puts "  Schedule: #{course.schedule}"
+        #puts "  Description: #{course.description}"
+     # end
+    #end
+  #end
 
 
