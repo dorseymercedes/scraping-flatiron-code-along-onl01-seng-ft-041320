c5d8f6e79e4d867b0b7dc29fa61546af5f1f6322
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-05-08 14:46:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,9 @@ require_relative './course.rb'
 
 class Scrape
     def get_page
-      
+          doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      binding.pry
   end
  
 end
